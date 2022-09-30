8524e073cec7672390ea9c4643083fa7af52b9f9
hermsale <hermsale@gmail.com>
2022-09-30 15:40:17 -0300

    generando metodos de axios

--- proyecto-consumo-api-rest/main.js
+++ proyecto-consumo-api-rest/main.js
@@ -1,6 +1,14 @@
+// const axios = require('axios');
 
-
+const api = axios.create({
+    baseURL: 'https://api.thecatapi.com/v1/',
+  });
+  
 const API_KEY = 'live_9vU1zH5wvwihzKOprl7XpQoBpTuLu5eEmYZUiYekMwtjykqGOj8AVrRmXtbqQeZv';
+
+// agregamos el default token - le indicamos el auth que nos indica la documentacion de la api 
+api.defaults.headers.common['X-API-KEY'] = API_KEY;
+
 
 const spanError = document.querySelector('.spanError');
 
@@ -9,7 +17,7 @@ const spanError = document.querySelector('.spanError');
 let queryParams = ['?','limit=3'].join('');
 
 const URL = `https://api.thecatapi.com/v1/images/search${queryParams}`;
-const URL_FAVORITES = `https://api.thecatapi.com/v1/favourites`;
+// const URL_FAVORITES = `https://api.thecatapi.com/v1/favourites`;
 const URL_FAVORITES_DELETE = (id) => `https://api.thecatapi.com/v1/favourites/${id}`;
 const URL_UPLOAD_IMAGE = `https://api.thecatapi.com/v1/images/upload`;
 
@@ -20,54 +28,43 @@ const img3 = document.getElementById('img3');
 
 
 async function reload(){
-    const response = await fetch(URL);
-    const data = await response.json();
-    console.log('data reload')
-    console.log(data);
 
-    if(response.status !== 200){
-        spanError.innerHTML = `<p>Ocurrio un problema</p>`+response.status;
-    }else{
+    try{
+        const res = await api.get('/images/search',{ params: { limit: 3 } });
+      
         const btnFavorites1 = document.querySelector('#btnFavorites1');
         const btnFavorites2 = document.querySelector('#btnFavorites2');
         const btnFavorites3 = document.querySelector('#btnFavorites3');
-        img1.src = data[0].url;
-        img2.src = data[1].url;
-        img3.src = data[2].url;
+        img1.src = res.data[0].url;
+        img2.src = res.data[1].url;
+        img3.src = res.data[2].url;
 
         // tenemos que agregar el arrow function para que no se ejecute todos los event onclick
-        btnFavorites1.onclick = () => addFavorites(data[0].id)
-        btnFavorites2.onclick = () => addFavorites(data[1].id)
-        btnFavorites3.onclick = () => addFavorites(data[2].id)
-    }
+        btnFavorites1.onclick = () => addFavorites(res.data[0].id)
+        btnFavorites2.onclick = () => addFavorites(res.data[1].id)
+        btnFavorites3.onclick = () => addFavorites(res.data[2].id)
 
+    }catch(err){
+        spanError.innerHTML = `<p>Ocurrio un problema</p> `+err;
+    }
+    
     loadFavorites();
-   
 }
 
 
 
-// endpoint favourites https://docs.thecatapi.com/favourites
+
 async function loadFavorites(){
     const imgFavoritas = document.getElementById('imgFavoritas');
     const imgfav = document.getElementById('imgfav');
 
-    const res = await fetch(URL_FAVORITES,{
-        method: 'GET',
-        headers: {
-            'X-API-KEY': `${API_KEY}`,
-        }
-    });
-    const data = await res.json();
+    try{
+        
+        const res = await api.get('/favourites',{ params: { limit: 30 } })
+        
+        console.log('data URL FAVORITES')
+        console.log(res);
     
-    console.log('data URL FAVORITES')
-    console.log(data);
-
-    if(res.status !== 200){
-        console.log('algo fallo');
-        spanError.innerHTML = '<p>Ocurrio un problema</p>'+response.status;
-    }else{
-        console.log('esta todo okey');
         // limpiamos el render
         imgFavoritas.innerHTML = '';
 
@@ -79,7 +76,7 @@ async function loadFavorites(){
         imgFavoritas.appendChild(h2);
         imgFavoritas.appendChild(imgfav);
 
-        data.map(img =>{
+        res.data.map(img =>{
             article = document.createElement('article'); 
             article.setAttribute('id','imgfav')
             imgElement = document.createElement('img');
@@ -97,8 +94,12 @@ async function loadFavorites(){
 
         })
 
+        console.log('se resolvio exitosamente');
+    
+    }catch(err){
+        console.log('algo fallo');
+        spanError.innerHTML = '<p>Ocurrio un problema</p>'+err;
     }
-
 }
 
 reload();
@@ -107,30 +108,19 @@ reload();
 async function addFavorites(id){
     console.log(id)
 
-    const response = await fetch(URL_FAVORITES,
-        {
-        method:'POST',
-        mode: 'cors',
-        headers:{
-            'X-API-KEY': `${API_KEY}`,
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ // transforma en JSON para que lo reciba el backend
-            image_id : id,
+    try{
+        let data = await api.post('/favourites',{
+            image_id:id
         }
-        )
-    });
-   
-    const data = await response.json();
-    
-    console.log(response.status);
-    console.log(data.message);
-    if(response.status !== 200){
-        console.log('algo fallo');
-        spanError.innerHTML = 'Ocurrio un problema'+response.status+data.message;
-    }else{
-        console.log('esta todo okey');
+        );
+        
+        
+        console.log('esta todo okey '+data.status);
         loadFavorites();
+    }catch(error){
+        console.log('algo fallo '+error);
+
+        // spanError.innerHTML = 'Ocurrio un problema'+status+data;
     }
 
 }
